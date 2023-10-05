d09de31870740350b9d5f8b76f29d1c21d1d9adb
Karen <karen@nulls-MacBook-Air.local>
2023-10-05 15:46:33 -0500

    chore: include css var

--- src/components/ComicViewer.vue
+++ src/components/ComicViewer.vue
@@ -3,13 +3,21 @@
     <h2 class="comic-title" v-if="currentComic">{{ currentComic.title }}</h2>
     <img class="comic-image" v-if="currentComic && currentComic.img" :src="currentComic.img" alt="Comic"/>
     <StarRating class="comic-rate" v-model:currentRating="userRating" :maxRating="5"/><span class="comic-rate__text">Rate this Comic</span>
-    <a class="load-next-comic-button" @click="incrementNum">
-      <span class="load-next-comic-button__text">Next Comic</span>
-      <svg class="load-next-comic-button__icon" width="13px" height="10px" viewBox="0 0 13 10">
-        <path d="M1,5 L11,5"></path>
-        <polyline points="8 1 12 5 8 9"></polyline>
-      </svg>
-    </a>
+    <div class="comic-container-button">
+      <a class="load-comic-button" @click="PreviousComic">
+        <span class="load-comic-button__text">Prev</span>
+        <svg class="load-comic-button__icon" width="13px" height="10px" viewBox="0 0 13 10">
+          <path d="M12,5 L2,5 M2,5 L6,1 M2,5 L6,9"></path>
+        </svg>
+      </a>
+      <a class="load-comic-button" @click="NextComic">
+        <span class="load-comic-button__text">Next</span>
+        <svg class="load-comic-button__icon" width="13px" height="10px" viewBox="0 0 13 10">
+          <path d="M1,5 L11,5"></path>
+          <polyline points="8 1 12 5 8 9"></polyline>
+        </svg>
+      </a>
+  </div>
   </div>
 </template>
 
@@ -26,7 +34,7 @@ export default {
     const comicStore = useComicStore();
     const currentComic = computed(() => comicStore.getCurrentComic);
     const userRating = ref(0);
-    const num = ref(1);
+    const num = ref(10);
     comicStore.fetchCurrentComic(100);
 
     const rateComic = () => {
@@ -35,35 +43,39 @@ export default {
       }
     };
 
-    const incrementNum = () => {
+    const NextComic = () => {
       num.value++;
       comicStore.fetchCurrentComic(num.value);
       userRating.value = 0;
     };
 
-    return { currentComic, userRating, rateComic, incrementNum };
+    const PreviousComic = () => {
+      num.value--;
+      comicStore.fetchCurrentComic(num.value);
+      userRating.value = 0;
+    };
+
+    return { currentComic, userRating, rateComic, NextComic, PreviousComic };
 
   }
 };
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&family=PT+Sans+Narrow&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Cormorant:wght@300&family=Libre+Baskerville:wght@700&family=PT+Sans+Narrow&display=swap');
 .comic-container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   text-align: center;
-  margin: 200px;
-  max-width: 300px;
-  height: 150px;
+  margin: var(--comic-container-margin);
+  max-width: var(--comic-container-max-width);
+  height: var(--comic-container-height);
 }
 
 .comic-title {
   font-family: 'Libre Baskerville', serif;
-  margin: 40px 0;
+  margin: var(--comic-title-margin);
   white-space: nowrap;
 }
 
@@ -73,52 +85,68 @@ export default {
 }
 .comic-rate__text {
   font-family: 'Cormorant', serif;
-  margin-bottom: 40px;
+  margin-bottom: var(--comic-rate-text-margin);
   white-space: nowrap;
 }
 .comic-image {
-  max-width: 300px;
+  max-width: var(--comic-image-max-width);
   height: 100%;
   object-fit: cover;
-  box-shadow: inset 1px 2px 17px 2px rgba(63,135,205,0.64);
-  margin-bottom: 30px;
+  box-shadow: var(--comic-image-shadow);
+  margin-bottom: var(--comic-image-margin-bottom);
 }
 
+.comic-container-button {
+  display: flex;
+  gap: var(--comic-button-gap);
+}
+
+@media (max-width: 600px) {
+  .comic-container-button {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+}
 a {
   text-decoration: none;
   color: inherit;
 }
-.load-next-comic-button {
+.load-comic-button {
   position: relative;
-  padding: 19px 22px;
-  transition: all 0.2s ease;
+  padding: var(--load-comic-padding);
+  transition: var(--load-comic-button-transition);
   cursor: pointer;
 }
-.load-next-comic-button:before {
+.load-comic-button:before {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
   content: "";
   position: absolute;
   top: 0;
   left: 0;
   display: block;
-  border-radius: 28px;
-  background: rgba(144, 194, 243, 0.5);
+  border-radius: var(--load-comic-button-border-radius);
+  background: var(--load-comic-button-background);
   width: 100%;
-  height: 56px;
-  transition: all 0.3s ease;
-  box-shadow: inset 1px 2px 17px 2px rgba(63,135,205,0.64);
+  height: var(--load-comic-button-height);
+  transition: var(--load-comic-button-transition);
+  box-shadow: var(--comic-shadow);
 }
-.load-next-comic-button__text {
+.load-comic-button__text {
   position: relative;
-  font-size: 16px;
-  line-height: 18px;
-  font-weight: 900;
-  letter-spacing: 0.25em;
-  text-transform: uppercase;
-  vertical-align: middle;
+  font-size: var(--load-comic-button-font-size);
+  line-height: var(--load-comic-button-line-height);
+  font-weight: var(--load-comic-button-font-weight);
+  letter-spacing: var(--load-comic-button-letter-spacing);
+  text-transform: var(--load-comic-button-text-transform);
+  vertical-align: var(--load-comic-button-vertical-align);
   white-space: nowrap;
 }
 
-.load-next-comic-button__icon {
+.load-comic-button__icon {
   position: relative;
   top: 0;
   margin-left: 10px;
@@ -128,22 +156,20 @@ a {
   stroke: #111;
   stroke-width: 2;
   transform: translateX(-5px);
-  transition: all 0.3s ease;
+  transition: var(--load-comic-button-transition);
 }
 
 @media (max-width: 767px) {
-  .load-next-comic-button__icon {
+  .load-comic-button__icon {
     display: none;
   }
 }
-.load-next-comic-button:hover:before {
+.load-comic-button:hover:before {
   width: 100%;
   background: #90C2F3;
 }
-.load-next-comic-button:hover svg {
-  transform: translateX(0);
-}
-.load-next-comic-button:active {
+
+.load-comic-button:active {
   transform: scale(0.96);
 }
 </style>
